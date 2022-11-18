6ca7345a1292fa4a657cc95202d964a3957e782c
dejmedus <email96123@gmail.com>
2022-11-18 14:26:37 -0700

    move prompt and artist array to config

--- main.py
+++ main.py
@@ -3,43 +3,44 @@ import webbrowser
 import requests
 from PIL import Image
 from datetime import datetime
-from config import API_Key, Org_ID
+from config import API_Key, Org_ID, Number_of_Generations, Prompt, Array_of_Artists
 
 
 openai.organization = Org_ID
 openai.api_key = API_Key
-date = datetime.now()
 
-num = 2
-prompt="rocky beach scene with a dragon flying over the ocean in the distance"
-artist_arr = ['Joaquín Sorolla', 'Ilya Repin', 'Frederic Edwin Church', 'Camille Pissarro']
+num = Number_of_Generations
+prompt = Prompt
+artist_arr = Array_of_Artists
+
+date = datetime.now()
+yes = ['yes', 'y']
+no = ['no', 'n']
 
 for artist in artist_arr:
     current_prompt = f'{prompt} by {artist}'
     response = openai.Image.create(
-    prompt=current_prompt,
-    n=num,
-    size="1024x1024" # "512x512"
+        prompt=current_prompt,
+        n=num,
+        size="1024x1024"  # "512x512"
     )
 
-    # variation 
+    # variation
     # response = openai.Image.create_variation(
     #   image=open(f'photos/{photoVar}', "rb"),
     #   n=num,
     #   size="1024x1024"
     # )
 
-
     f = open("hist.txt", "a")
     # sf = open("saved_hist.txt", "a")
-    yes = ['yes', 'y']
-    no = ['no', 'n']
 
     for i in range(num):
         url = response['data'][i]['url']
-        f.write(f'{i + 1} of {num}\n{current_prompt}\n{date.day} {date.hour}:{date.minute}\n{url}\n\n')
+        f.write(
+            f'{i + 1} of {num}\n{current_prompt}\n{date.day} {date.hour}:{date.minute}\n{url}\n\n')
         webbrowser.open_new_tab(url)
-        
+
         while True:
             save_image = input(f'\nSave {current_prompt} (yes/no): ')
             if save_image.lower() in yes:
@@ -49,8 +50,8 @@ for artist in artist_arr:
                 # name = '-'.join(name)
                 # name = f'{name}{i + 1}.png'
                 name = f'photos/{current_prompt} {i + 1}.png'
-                img = Image.open(requests.get(url, stream = True).raw)
-                
+                img = Image.open(requests.get(url, stream=True).raw)
+
                 # resize photo
                 # width, height = 256, 256
                 # image = img.resize((width, height))
@@ -62,6 +63,5 @@ for artist in artist_arr:
                 print('Please input yes or no')
                 continue
 
-    
     f.close()
     # sf.close()
